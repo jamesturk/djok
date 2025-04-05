cf2677c7ba2c875d53df1556162983aa98d8dfa0
jpt <dev@jpt.sh>
2025-04-05 17:11:04 -0500

    more config

--- README.md
+++ README.md
@@ -8,3 +8,38 @@
 
 Ignored:
 - _staticfiles
+
+## Projects
+
+### environ
+
+Configured per instructions, used to set up settings.
+
+### whitenoise
+
+Configured per instructions.
+
+### django-typer
+
+minimal config; done
+
+### django-structlog
+
+TODO: needs defaults
+
+### django-allauth
+
+Configured for email login.
+
+### pytest-django
+
+???
+
+### django-debug-toolbar
+
+Configured per instructions.
+
+## Options
+
+TODO: whitenoise Compression setting
+TODO: prerolled Auth options
--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,10 @@ DATABASES = {"default": env.db()}
 
 
 ALLOWED_HOSTS = []
+INTERNAL_IPS = ["127.0.0.1"]
+
+# Debug Toolbar
+IS_TESTING = "test" in sys.argv
 
 
 # Static Settings ------
@@ -31,6 +35,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
+    "allauth",
+    "allauth.account",
+    "django_structlog",
+    "django_typer",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -42,7 +51,16 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_structlog.middlewares.RequestMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
+
+if DEBUG and not IS_TESTING:
+    INSTALLED_APPS += "debug_toolbar"
+    MIDDLEWARE.insert(
+        2,
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    )
 
 TEMPLATES = [
     {
@@ -61,7 +79,18 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 ROOT_URLCONF = "config.urls"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
 
+
+# Authentication -----
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -78,12 +107,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
-USE_I18N = True
-USE_TZ = True
-
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION_BY_CODE_ENABLED = True
+ACCOUNT_EMAIL_UNKNOWN_ACCOUNTS = False
+ACCOUNT_LOGIN_BY_CODE_ENABLED = True
+ACCOUNT_LOGIN_METHODS = {"email"}
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
+ACCOUNT_PRESERVE_USERNAME_CASING = False
+ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
+ACCOUNT_SIGNUP_FORM_HONEYPOT_FIELD = "user_name"
+ACCOUNT_USERNAME_BLACKLIST = ["admin"]
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+# ACCOUNT_SIGNUP_FORM_CLASS = ""
+# ACCOUNT_EMAIL_SUBJECT_PREFIX = "[Site] "
+# ACCOUNT_LOGIN_BY_CODE_REQUIRED = False
+# ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
 
 # Static File Config (per whitenoise) -----
 
